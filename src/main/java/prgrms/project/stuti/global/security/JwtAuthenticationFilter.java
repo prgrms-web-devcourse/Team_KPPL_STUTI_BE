eddbb55eb2bc8e928ac3cd421118d483d25d52c2
Yonghoon Lee <dldydgns53@naver.com>
2022-07-28 00:21:46 +0900

    feat: security 설정을 적용하라

---
+++ src/main/java/prgrms/project/stuti/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,96 @@
+package prgrms.project.stuti.global.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import prgrms.project.stuti.global.cache.model.BlackListToken;
+import prgrms.project.stuti.global.cache.model.RefreshToken;
+import prgrms.project.stuti.global.cache.repository.RefreshTokenRepository;
+import prgrms.project.stuti.global.cache.service.BlackListTokenService;
+import prgrms.project.stuti.global.token.TokenGenerator;
+import prgrms.project.stuti.global.token.TokenService;
+import prgrms.project.stuti.global.token.TokenType;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final TokenService tokenService;
+	private final TokenGenerator tokenGenerator;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final BlackListTokenService blackListTokenService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		String token = tokenService.resolveToken((HttpServletRequest) request);
+
+		// 토큰이 있는지, 유효한지 검증
+		if (token != null && tokenService.verifyToken(token)) {
+			// 블랙리스트에 존재하는 토큰인지 체크
+			checkBlackList(token);
+
+			// 토큰에서 email 과 role 를 가져온다.
+			String email = tokenService.getUid(token);
+			String[] roles = tokenService.getRole(token);
+
+			setAuthenticationToSecurityCotextHolder(email, roles);
+
+		} else if (token != null) {
+			// 토큰이 유효하지 않은경우
+			// refresh token 을 redis 에서 찾은 후 존재하는 경우 accessToken 을 재발급하여 제공한다.
+			Optional<RefreshToken> optionalRefreshToken = refreshTokenRepository.findById(token);
+			if (optionalRefreshToken.isPresent()) {
+				RefreshToken refreshToken = optionalRefreshToken.get();
+				String email = tokenService.getUid(refreshToken.getRefreshTokenValue());
+				String[] role = tokenService.getRole(refreshToken.getRefreshTokenValue());
+
+				// accessToken 을 매핑되는 refreshToken 으로 갱신한 후 cookie 에 담은 후 contextholder 에 등록한다.
+				String newAccessToken = tokenGenerator.generateAccessToken(email, role);
+				setAuthenticationToSecurityCotextHolder(email, role);
+				tokenService.addAccessTokenToCookie(response, newAccessToken, TokenType.JWT_TYPE);
+			}
+		}
+		// 토큰이 유효하지 않은경우 다음 필터로 이동한다.
+		filterChain.doFilter(request, response);
+
+	}
+
+	private void checkBlackList(String token) {
+		Optional<BlackListToken> blackListToken = blackListTokenService.findById(
+			tokenService.tokenWithType(token, TokenType.JWT_BLACKLIST));
+		if (blackListToken.isPresent()) {
+			log.error("logout error - attack detected");
+			throw new IllegalArgumentException("logout error"); // login 으로 전달
+		}
+	}
+
+
+	private void setAuthenticationToSecurityCotextHolder(String email, String[] roles) {
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		Arrays.stream(roles).forEach(role -> {
+			authorities.add(new SimpleGrantedAuthority(role));
+		});
+
+		UsernamePasswordAuthenticationToken authenticationToken =
+			new UsernamePasswordAuthenticationToken(email, null, authorities);
+		// SecurityContextHolder에 설정한다. - 이곳을 통해 thread 당 해당 유저의 정보를 확인
+		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+	}
+}
---
+++ src/main/java/prgrms/project/stuti/global/security/OAuth2SuccessHandler.java
@@ -0,0 +1,123 @@
+package prgrms.project.stuti.global.security;
+
+import static org.springframework.http.HttpHeaders.*;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import prgrms.project.stuti.domain.member.model.Email;
+import prgrms.project.stuti.domain.member.model.Member;
+import prgrms.project.stuti.domain.member.model.MemberRole;
+import prgrms.project.stuti.domain.member.service.MemberService;
+import prgrms.project.stuti.global.cache.model.RefreshToken;
+import prgrms.project.stuti.global.cache.model.TemporaryMember;
+import prgrms.project.stuti.global.cache.repository.RefreshTokenRepository;
+import prgrms.project.stuti.global.cache.repository.TemporaryMemberRepository;
+import prgrms.project.stuti.global.token.TokenGenerator;
+import prgrms.project.stuti.global.token.TokenService;
+import prgrms.project.stuti.global.token.TokenType;
+import prgrms.project.stuti.global.token.Tokens;
+import prgrms.project.stuti.global.util.CoderUtil;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+	private final TokenGenerator tokenGenerator;
+	private final TokenService tokenService;
+	private final MemberService memberService;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final TemporaryMemberRepository temporaryMemberRepository;
+	private RedirectStrategy redirectStratgy = new DefaultRedirectStrategy();
+
+	@Value("${app.oauth.domain}")
+	private String domain;
+
+	@Value("${app.oauth.signupPath}")
+	private String signupPath;
+
+	@Value("${app.oauth.loginSuccessPath}")
+	private String loginSuccessPath;
+
+	@Value("${app.oauth.signupTime}")
+	private Long signupTime;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) throws IOException, ServletException {
+		// 인증 된 principal 를 가지고 온다.
+		OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+		Map<String, Object> attributes = oAuth2User.getAttributes();
+		String email = (String) attributes.get("email");
+		String name = (String) attributes.get("name");
+		String picture = (String) attributes.get("picture");
+
+		// 최초 로그인이라면 추가 회원가입 처리를 한다.
+		Optional<Member> optionalMember = memberService.getUser(new Email(email));
+		if (!optionalMember.isPresent()) {
+			TemporaryMember temporaryMember = new TemporaryMember(email, name, picture, signupTime);
+			temporaryMemberRepository.save(temporaryMember);
+
+			String temporaryMemberEmail = temporaryMember.getEmail();
+
+			String param1 = "?email=" + CoderUtil.encode(temporaryMemberEmail);
+			String param2 = "&name=" + CoderUtil.encode(name);
+			String targetURI = domain + signupPath + param1 + param2;
+			redirectStratgy.sendRedirect(request, response, targetURI);
+		} else {
+			// 이미 회원가입 한 유저의 경우 토큰을 refreshToken 저장 후 accessToken 쿠키 전달
+			Tokens tokens = tokenGenerator.generateTokens(email, MemberRole.ROLE_USER.stringValue);
+			saveRefreshTokenToRedis(tokens);
+
+			// cookie 로 전달
+			System.out.println(tokens.getAccessToken());
+			addAccessTokenToCookie(response, tokens.getAccessToken(), TokenType.JWT_TYPE);
+			redirectStratgy.sendRedirect(request, response, domain + loginSuccessPath);
+		}
+	}
+
+	private void saveRefreshTokenToRedis(Tokens tokens) {
+		Date now = new Date();
+		RefreshToken refreshToken = new RefreshToken(
+			tokens.getAccessToken(),
+			tokens.getRefreshToken(),
+			now,
+			new Date(now.getTime() + tokenService.getRefreshPeriod())
+		);
+		refreshTokenRepository.save(refreshToken);
+	}
+
+
+	private void addAccessTokenToCookie(HttpServletResponse response, String accessToken,
+		TokenType tokenType) {
+		Cookie cookie = new Cookie(AUTHORIZATION,
+			URLEncoder.encode(tokenService.tokenWithType(accessToken, tokenType),
+				StandardCharsets.UTF_8));
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge((int) tokenService.getAccessTokenPeriod());
+		cookie.setPath(loginSuccessPath);
+
+		response.addCookie(cookie);
+	}
+}

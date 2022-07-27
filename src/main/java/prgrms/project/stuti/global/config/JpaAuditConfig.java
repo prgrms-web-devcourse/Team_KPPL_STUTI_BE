cd2e192ecd402cc3e633f2a8ca648266feb4d1bf
Yonghoon Lee <dldydgns53@naver.com>
2022-07-28 00:17:20 +0900

    feat: jpaAudit, redis, cors 설정하라

--- src/main/java/prgrms/project/stuti/global/config/JpaAuditConfig.java
+++ src/main/java/prgrms/project/stuti/global/config/JpaAuditConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableJpaAuditing
@@ -13,6 +17,23 @@ public class JpaAuditConfig {
 
 	@Bean
 	public AuditorAware<String> auditorAware() {
-		return () -> Optional.of("test");
+		return () -> {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+			if (authentication == null || !authentication.isAuthenticated() || isAnonymous(
+				authentication)) {
+				return Optional.empty();
+			}
+
+			UsernamePasswordAuthenticationToken userToken = (UsernamePasswordAuthenticationToken) authentication.getPrincipal();
+			String email = (String) userToken.getPrincipal();
+
+			return Optional.ofNullable(email);
+		};
 	}
+
+	private boolean isAnonymous(Authentication authentication) {
+		return authentication instanceof AnonymousAuthenticationToken;
+	}
+
 }

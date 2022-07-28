dbd3781e269e043fb76ad2c2b08f57f01d2736dc
Yonghoon Lee <dldydgns53@naver.com>
2022-07-29 02:39:35 +0900

    refactor: 테스트 예외클래스를 변경하라

--- src/test/java/prgrms/project/stuti/domain/member/model/EmailTest.java
+++ src/test/java/prgrms/project/stuti/domain/member/model/EmailTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import prgrms.project.stuti.global.error.exception.TokenException;
+
 class EmailTest {
 	@ParameterizedTest
 	@DisplayName("이메일 형식에 맞지않으면 예외가 발생한다")
 	@ValueSource(strings = {"user@gmail", "usergmail", "@gmail.com", "user@.com"})
 	void emailValidationTest(String email) {
-		assertThrows(IllegalArgumentException.class, () -> new Email(email));
+		assertThrows(TokenException.class, () -> new Email(email));
 	}
 
 	@Test

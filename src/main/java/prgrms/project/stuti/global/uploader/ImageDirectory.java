ca337f3e26eec890c5112878f2ecdf3985d13c46
Gosh95 <goshk1995@gmail.com>
2022-07-29 00:38:29 +0900

    refactor: 사용하지 않는 디렉터리 enum 변수를 제거하라

--- src/main/java/prgrms/project/stuti/global/uploader/ImageDirectory.java
+++ src/main/java/prgrms/project/stuti/global/uploader/ImageDirectory.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ImageDirectory {
-	MEMBER("images/members"), FEED("images/feeds"), STUDY_GROUP("images/study-groups"), THUMBNAIL("images/thumbnails");
+	MEMBER("images/members"), FEED("images/feeds"), STUDY_GROUP("images/study-groups");
 
 	private final String directory;
 

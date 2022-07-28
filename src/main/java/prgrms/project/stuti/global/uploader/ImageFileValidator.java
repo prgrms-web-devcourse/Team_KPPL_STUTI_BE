695245695f786e40d73eea61268763f0ec1dcb8f
Gosh95 <goshk1995@gmail.com>
2022-07-28 17:04:35 +0900

    feat: 이미지 파일 validator 를 구현하라

---
+++ src/main/java/prgrms/project/stuti/global/uploader/ImageFileValidator.java
@@ -0,0 +1,35 @@
+package prgrms.project.stuti.global.uploader;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import prgrms.project.stuti.global.error.exception.FileException;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ImageFileValidator {
+
+	private static final List<String> SUPPORTED_CONTENT_TYPES = List.of("image/png", "image/jpeg", "image/jpg");
+
+	public static void validateImageFile(MultipartFile imageFile) {
+		validateEmptyValue(imageFile);
+		validateContentType(imageFile);
+	}
+
+	private static void validateEmptyValue(MultipartFile imageFile) {
+		if (imageFile.isEmpty()) {
+			FileException.EMPTY_FILE.get();
+		}
+	}
+
+	private static void validateContentType(MultipartFile imageFile) {
+		String contentType = imageFile.getContentType();
+		boolean isSupported = SUPPORTED_CONTENT_TYPES.contains(contentType);
+
+		if (!isSupported) {
+			FileException.UNSUPPORTED_EXTENSION.accept(contentType);
+		}
+	}
+}

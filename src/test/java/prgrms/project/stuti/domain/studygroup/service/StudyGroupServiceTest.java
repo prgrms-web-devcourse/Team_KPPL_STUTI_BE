9fd362b0aa314743baed1fcf7e69f61eadad2c93
Gosh95 <goshk1995@gmail.com>
2022-08-01 01:35:39 +0900

    feat: 스터디 그룹 서비스의 생성 기능을 구현하라

---
+++ src/test/java/prgrms/project/stuti/domain/studygroup/service/StudyGroupServiceTest.java
@@ -0,0 +1,96 @@
+package prgrms.project.stuti.domain.studygroup.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import prgrms.project.stuti.domain.member.model.Career;
+import prgrms.project.stuti.domain.member.model.Field;
+import prgrms.project.stuti.domain.member.model.Mbti;
+import prgrms.project.stuti.domain.member.model.Member;
+import prgrms.project.stuti.domain.member.model.MemberRole;
+import prgrms.project.stuti.domain.member.repository.MemberRepository;
+import prgrms.project.stuti.domain.studygroup.model.Region;
+import prgrms.project.stuti.domain.studygroup.model.Topic;
+import prgrms.project.stuti.domain.studygroup.service.dto.StudyGroupCreateDto;
+import prgrms.project.stuti.domain.studygroup.service.dto.StudyGroupIdResponse;
+
+@SpringBootTest
+class StudyGroupServiceTest {
+
+	@Autowired
+	private StudyGroupService studyGroupService;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private static Member member;
+
+	@BeforeEach
+	void setup() {
+		Member testMember = Member.builder()
+			.email("test@gmail.com")
+			.nickName("nickname")
+			.field(Field.BACKEND)
+			.career(Career.JUNIOR)
+			.profileImageUrl("www.aws.s3")
+			.mbti(Mbti.ENFJ)
+			.githubUrl("www.github.com")
+			.blogUrl("www.blog.com")
+			.memberRole(MemberRole.ROLE_MEMBER)
+			.build();
+
+		member = memberRepository.save(testMember);
+	}
+
+	@Test
+	@DisplayName("새로운 스터디 그룹을 생성한다.")
+	void testCreateStudyGroup() throws IOException {
+		StudyGroupCreateDto createDto = toCreateDto(member.getId());
+		StudyGroupIdResponse idResponse = studyGroupService.createStudyGroup(createDto);
+
+		assertNotNull(idResponse);
+		assertEquals(1L, idResponse.studyGroupId());
+	}
+
+	private StudyGroupCreateDto toCreateDto(Long memberId) throws IOException {
+		return StudyGroupCreateDto
+			.builder()
+			.memberId(memberId)
+			.imageFile(getMultipartFile())
+			.title("title")
+			.topic(Topic.AI)
+			.isOnline(false)
+			.region(Region.SEOUL)
+			.preferredMbtis(List.of(Mbti.ENFJ, Mbti.ENFP))
+			.numberOfRecruits(5)
+			.startDateTime(LocalDateTime.now().plusDays(10))
+			.endDateTime(LocalDateTime.now().plusMonths(3))
+			.description("this is new study group")
+			.build();
+	}
+
+	private MultipartFile getMultipartFile() throws IOException {
+		File imageFile = new File(Paths.get("src", "test", "resources") + File.separator + "test.png");
+		FileInputStream inputStream = new FileInputStream(imageFile);
+		String[] split = imageFile.getName().split("\\.");
+
+		return new MockMultipartFile(split[0], imageFile.getName(), "image/" + split[1], inputStream);
+	}
+}

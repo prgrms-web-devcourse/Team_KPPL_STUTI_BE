7b93801cf62ed961ebf1cb02b70e6a4e0e184519
Gosh95 <goshk1995@gmail.com>
2022-08-01 16:52:32 +0900

    feat: 스터디 그룹 컨트롤러의 업데이트 기능을 구현하라

--- src/main/java/prgrms/project/stuti/domain/studygroup/controller/StudyGroupRestController.java
+++ src/main/java/prgrms/project/stuti/domain/studygroup/controller/StudyGroupRestController.java
@@ -7,15 +7,19 @@ import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import prgrms.project.stuti.domain.studygroup.controller.dto.StudyGroupCreateRequest;
+import prgrms.project.stuti.domain.studygroup.controller.dto.StudyGroupUpdateRequest;
 import prgrms.project.stuti.domain.studygroup.service.StudyGroupService;
 import prgrms.project.stuti.domain.studygroup.service.dto.StudyGroupCreateDto;
 import prgrms.project.stuti.domain.studygroup.service.dto.StudyGroupIdResponse;
+import prgrms.project.stuti.domain.studygroup.service.dto.StudyGroupUpdateDto;
 
 @RestController
 @RequestMapping("/api/v1/study-groups")
@@ -32,5 +36,14 @@ public class StudyGroupRestController {
 		URI uri = URI.create("/api/v1/study-groups/" + idResponse.studyGroupId());
 
 		return ResponseEntity.created(uri).body(idResponse);
+	}
+
+	@PatchMapping("/{studyGroupId}")
+	public ResponseEntity<StudyGroupIdResponse> updateStudyGroup(@AuthenticationPrincipal Long memberId,
+		@PathVariable Long studyGroupId, @Valid @ModelAttribute StudyGroupUpdateRequest updateRequest) {
+		StudyGroupUpdateDto updateDto = StudyGroupMapper.toStudyGroupUpdateDto(memberId, studyGroupId, updateRequest);
+		StudyGroupIdResponse idResponse = studyGroupService.updateStudyGroup(updateDto);
+
+		return ResponseEntity.ok(idResponse);
 	}
 }

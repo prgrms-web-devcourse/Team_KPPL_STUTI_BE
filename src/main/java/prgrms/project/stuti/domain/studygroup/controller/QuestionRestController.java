f745dde6d64a4e159bedade2904631a06a291798
Gosh95 <goshk1995@gmail.com>
2022-08-03 16:01:14 +0900

    feat: 질문 컨트롤러의 삭제 기능을 구현하라

--- src/main/java/prgrms/project/stuti/domain/studygroup/controller/QuestionRestController.java
+++ src/main/java/prgrms/project/stuti/domain/studygroup/controller/QuestionRestController.java
@@ -4,8 +4,10 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -47,5 +49,13 @@ public class QuestionRestController {
 		QuestionIdResponse idResponse = questionService.updateQuestion(updateDto);
 
 		return ResponseEntity.ok(idResponse);
+	}
+
+	@DeleteMapping("/{questionId}")
+	public ResponseEntity<Void> deleteQuestion(@AuthenticationPrincipal Long memberId,
+		@PathVariable Long studyGroupId, @PathVariable Long questionId) {
+		questionService.deleteQuestion(memberId, studyGroupId, questionId);
+
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).build();
 	}
 }

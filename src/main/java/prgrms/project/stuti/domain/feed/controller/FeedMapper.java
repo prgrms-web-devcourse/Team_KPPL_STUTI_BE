3e725119e51dba74693cea88369a118f03a724eb
yerimmac <yerim.lee.earth@gmail.com>
2022-08-03 18:33:48 +0900

    refactor: request에 맞춰 class이름과 변수명을 변경하라

--- src/main/java/prgrms/project/stuti/domain/feed/controller/FeedMapper.java
+++ src/main/java/prgrms/project/stuti/domain/feed/controller/FeedMapper.java
@@ -8,16 +8,16 @@ public class FeedMapper {
 	public static PostCreateDto toPostCreateDto(RegisterPostRequest registerPostRequest, Long memberId) {
 		return PostCreateDto.builder()
 			.memberId(memberId)
-			.contents(registerPostRequest.content())
-			.imageFile(registerPostRequest.imageFile())
+			.contents(registerPostRequest.contents())
+			.imageFile(registerPostRequest.postImage())
 			.build();
 	}
 
 	public static PostChangeDto toPostChangeDto(RegisterPostRequest registerPostRequest, Long postId) {
 		return PostChangeDto.builder()
 			.postId(postId)
-			.contents(registerPostRequest.content())
-			.imageFile(registerPostRequest.imageFile())
+			.contents(registerPostRequest.contents())
+			.imageFile(registerPostRequest.postImage())
 			.build();
 	}
 
--- src/main/java/prgrms/project/stuti/domain/feed/controller/dto/PostRequest.java
+++ src/main/java/prgrms/project/stuti/domain/feed/controller/dto/PostRequest.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record RegisterPostRequest(
-	MultipartFile imageFile,
-	@NotNull String content
+	MultipartFile postImage,
+	@NotNull String contents
 ) {
 }

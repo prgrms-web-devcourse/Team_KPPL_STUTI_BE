a3f35d7ef254a21a8ac64616527237e5a6dd24a2
yerimmac <yerim.lee.earth@gmail.com>
2022-08-11 01:35:02 +0900

    docs: 적용될 sql문을 등록하라

---
+++ src/main/resources/db/migration/R__009_POST.sql
@@ -0,0 +1,33 @@
+INSERT INTO kppl.POST (POST_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID) VALUES (99, '2022-08-08 23:56:36.982815', '2022-08-08 23:56:36.982815', -1, -1, '님은 갔습니다. 아아 사랑하는 나의 님은 갔습니다.
+푸른 산빛을 깨치고, 단풍나무 숲을 향하여 난, 작은 길을 걸어서 차마 떨치고 갔습니다.
+황금의 꽃같이 굳고 빛나던 옛 맹서는 차디찬 티끌이 되어서, 한숨의 미풍에 날아갔습니다.
+날카로운 첫 「키스」의 추억은 나의 운명의 지침을 돌려놓고, 뒷걸음쳐서 사라졌습니다.
+나는 향기로운 님의 말소리에 귀먹고, 꽃다운 님의 얼굴에 눈멀었습니다.
+사랑도 사람의 일이라, 만날 때에 미리 떠날 것을 염려하고 경계하지 아니한 것은 아니지만, 이별은 뜻밖의 일이 되고 놀란 가슴은 새로운 슬픔에 터집니다.
+그러나 이별을 쓸데없는 눈물의 원천을 만들고 마는 것은 스스로 사랑을 깨뜨리는 것인 줄 아는 까닭에, 걷잡을 수 없는 슬픔의 힘을 옮겨서 새 희망의 정수박이에 들이부었습니다.
+우리는 만날 때에 떠날 것을 염려하는 것과 같이, 떠날 때에 다시 만날 것을 믿습니다.
+아아 님은 갔지마는 나는 님을 보내지 아니하였습니다.
+제 곡조를 못 이기는 사랑의 노래는 님의 침묵을 휩싸고 돕니다.', 6);
+INSERT INTO kppl.POST (POST_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID) VALUES (100, '2022-08-08 23:56:36.991019', '2022-08-08 23:56:36.991019', -1, -1, '님은 갔습니다. 아아 사랑하는 나의 님은 갔습니다.
+푸른 산빛을 깨치고, 단풍나무 숲을 향하여 난, 작은 길을 걸어서 차마 떨치고 갔습니다.
+황금의 꽃같이 굳고 빛나던 옛 맹서는 차디찬 티끌이 되어서, 한숨의 미풍에 날아갔습니다.
+날카로운 첫 「키스」의 추억은 나의 운명의 지침을 돌려놓고, 뒷걸음쳐서 사라졌습니다.
+나는 향기로운 님의 말소리에 귀먹고, 꽃다운 님의 얼굴에 눈멀었습니다.
+사랑도 사람의 일이라, 만날 때에 미리 떠날 것을 염려하고 경계하지 아니한 것은 아니지만, 이별은 뜻밖의 일이 되고 놀란 가슴은 새로운 슬픔에 터집니다.
+그러나 이별을 쓸데없는 눈물의 원천을 만들고 마는 것은 스스로 사랑을 깨뜨리는 것인 줄 아는 까닭에, 걷잡을 수 없는 슬픔의 힘을 옮겨서 새 희망의 정수박이에 들이부었습니다.
+우리는 만날 때에 떠날 것을 염려하는 것과 같이, 떠날 때에 다시 만날 것을 믿습니다.
+아아 님은 갔지마는 나는 님을 보내지 아니하였습니다.
+제 곡조를 못 이기는 사랑의 노래는 님의 침묵을 휩싸고 돕니다.', 6);
+
+
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4000, '2022-08-08 23:56:38.444373', '2022-08-08 23:56:38.444373', -1, -1, '게시글1에대한 댓글1', 2, null, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4001, '2022-08-08 23:56:38.465055', '2022-08-08 23:56:38.465055', -1, -1, '게시글1에대한 댓글1에 대한 대댓글1', 1, 4000, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4002, '2022-08-08 23:56:38.471122', '2022-08-08 23:56:38.471122', -1, -1, '게시글1에대한 댓글1에 대한 대댓글2', 2, 4000, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4003, '2022-08-08 23:56:38.477303', '2022-08-08 23:56:38.477303', -1, -1, '게시글1에대한 댓글1에 대한 대댓글3', 3, 4000, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4004, '2022-08-08 23:56:38.483593', '2022-08-08 23:56:38.483593', -1, -1, '게시글1에대한 댓글1에 대한 대댓글4', 4, 4000, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4005, '2022-08-08 23:56:38.492059', '2022-08-08 23:56:38.492059', -1, -1, '게시글1에대한 댓글2', 3, null, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4006, '2022-08-08 23:56:38.498679', '2022-08-08 23:56:38.498679', -1, -1, '게시글1에대한 댓글2에 대한 대댓글1', 1, 4005, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4007, '2022-08-08 23:56:38.505093', '2022-08-08 23:56:38.505093', -1, -1, '게시글1에대한 댓글2에 대한 대댓글2', 2, 4005, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4008, '2022-08-08 23:56:38.510384', '2022-08-08 23:56:38.510384', -1, -1, '게시글1에대한 댓글2에 대한 대댓글3', 3, 4005, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4009, '2022-08-08 23:56:38.516522', '2022-08-08 23:56:38.516522', -1, -1, '게시글1에대한 댓글2에 대한 대댓글4', 4, 4005, 99);
+INSERT INTO kppl.POST_COMMENT (POST_COMMENT_ID, CREATED_AT, UPDATED_AT, CREATED_BY, UPDATED_BY, CONTENTS, MEMBER_ID, PARENT_ID, POST_ID) VALUES (4010, '2022-08-08 23:56:38.521689', '2022-08-08 23:56:38.521689', -1, -1, '게시글1에대한 댓글3', 4, null, 99);

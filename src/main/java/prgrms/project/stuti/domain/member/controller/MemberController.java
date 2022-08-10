3442bf13351979186b45d2d96c47ee10ce5f1fd7
Yonghoon Lee <dldydgns53@naver.com>
2022-08-11 02:14:12 +0900

    feat: s3 설정을 주석처리하라

--- src/main/java/prgrms/project/stuti/domain/member/controller/MemberController.java
+++ src/main/java/prgrms/project/stuti/domain/member/controller/MemberController.java
@@ -33,7 +33,8 @@ public class MemberController {
 	@PutMapping("/{memberId}")
 	public ResponseEntity<MemberResponse> member(@PathVariable Long memberId,
 		@Valid @RequestBody MemberPutRequest memberPutRequest) {
-		MemberResponse memberResponse = memberService.editMember(memberId, MemberMapper.toMemberPutDto(memberPutRequest));
+		MemberResponse memberResponse = memberService.editMember(memberId,
+			MemberMapper.toMemberPutDto(memberPutRequest));
 		URI uri = URI.create("/api/v1/members/" + memberId);
 
 		return ResponseEntity

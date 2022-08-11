325734730653b747230440491134744519950856
Gosh95 <goshk1995@gmail.com>
2022-08-11 14:41:28 +0900

    feat: Aws s3 이미지 업로드 기능을 구현하라

--- src/main/java/prgrms/project/stuti/global/uploader/AwsS3ImageUploader.java
+++ src/main/java/prgrms/project/stuti/global/uploader/AwsS3ImageUploader.java
@@ -1,27 +1,105 @@
 package prgrms.project.stuti.global.uploader;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import prgrms.project.stuti.global.uploader.common.ImageDirectory;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
-//TODO: 2022/07/27 배포 환경에서 사용
+import lombok.RequiredArgsConstructor;
+import prgrms.project.stuti.global.error.exception.FileException;
+import prgrms.project.stuti.global.uploader.common.ImageDirectory;
+import prgrms.project.stuti.global.uploader.common.ImageFileUtils;
+
+@Primary
+@Service
+@RequiredArgsConstructor
 public class AwsS3ImageUploader implements ImageUploader {
+
+	private final AmazonS3Client amazonS3Client;
+
+	@Value("${could.aws.s3.bucket}")
+	private String bucket;
+
+	private final String beginDirectory = "images";
 
 	@Override
 	public String upload(MultipartFile multipartFile, ImageDirectory imageDirectory) {
-		return Strings.EMPTY;
+		if (multipartFile == null || multipartFile.isEmpty()) {
+			return Strings.EMPTY;
+		}
+
+		String imageFileName = createImageFileName(multipartFile, imageDirectory);
+		uploadToS3(multipartFile, imageFileName);
+
+		return getImageUrl(imageFileName);
 	}
 
 	@Override
 	public List<String> uploadAll(List<MultipartFile> multipartFiles, ImageDirectory imageDirectory) {
-		return Collections.emptyList();
+		if (multipartFiles == null || multipartFiles.isEmpty()) {
+			return List.of(Strings.EMPTY);
+		}
+
+		List<String> imageUrls = Collections.synchronizedList(new ArrayList<>());
+
+		for (MultipartFile multipartFile : multipartFiles) {
+			String imageFileName = createImageFileName(multipartFile, imageDirectory);
+			uploadToS3(multipartFile, imageFileName);
+
+			imageUrls.add(getImageUrl(imageFileName));
+		}
+
+		return imageUrls;
 	}
 
 	@Override
 	public void delete(String imageUrl) {
+		String imageFileName = extractImageFileName(imageUrl);
+		amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, imageFileName));
+	}
+
+	private String createImageFileName(MultipartFile multipartFile, ImageDirectory imageDirectory) {
+		String extension = ImageFileUtils.getExtension(Objects.requireNonNull(multipartFile.getContentType()));
+
+		return imageDirectory.getDirectory() + File.separator + ImageFileUtils.rename(extension);
+	}
+
+	private void uploadToS3(MultipartFile multipartFile, String imageFileName) {
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentLength(multipartFile.getSize());
+		objectMetadata.setContentType(multipartFile.getContentType());
+
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+			amazonS3Client.putObject(
+				new PutObjectRequest(bucket, imageFileName, inputStream, objectMetadata)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+		} catch (IOException ex) {
+			throw FileException.failedToUpload(ex);
+		}
+	}
+
+	private String getImageUrl(String imageFileName) {
+		return String.valueOf(amazonS3Client.getUrl(bucket, imageFileName));
+	}
+
+	private String extractImageFileName(String imageUrl) {
+		int beginIndex = imageUrl.lastIndexOf(beginDirectory);
+
+		return imageUrl.substring(beginIndex);
 	}
 }

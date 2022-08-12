58efbe395f5e61d06661f7506c8de9db6f63ef0b
Yonghoon Lee <dldydgns53@naver.com>
2022-08-12 23:25:22 +0900

    chore: 누락된 import 추가하라

--- src/main/java/prgrms/project/stuti/domain/member/controller/AuthenticationController.java
+++ src/main/java/prgrms/project/stuti/domain/member/controller/AuthenticationController.java
@@ -5,9 +5,10 @@ import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

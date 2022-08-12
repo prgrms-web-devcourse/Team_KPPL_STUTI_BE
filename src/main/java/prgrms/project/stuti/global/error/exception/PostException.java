f80f35bfc5ad70648f7b3b3bb7e3e9560951d586
yerimmac <yerim.lee.earth@gmail.com>
2022-08-12 17:35:30 +0900

    refactor: exception을 throw가 아닌 return하도록 수정하라

--- src/main/java/prgrms/project/stuti/global/error/exception/PostException.java
+++ src/main/java/prgrms/project/stuti/global/error/exception/PostException.java
@@ -11,34 +11,34 @@ public class PostException extends BusinessException {
 	}
 
 	public static PostException POST_NOT_FOUND(Long postId) {
-		throw new PostException(ErrorCode.NOT_FOUND_POST,
+		return new PostException(ErrorCode.NOT_FOUND_POST,
 			MessageFormat.format("존재하지 않는 게시글입니다. (postId: {0})", postId));
 	}
 
 	public static PostException POST_LIKE_DUPLICATED(Long postLikeId) {
-		throw new PostException(ErrorCode.POST_LIKE_DUPLICATED,
+		return new PostException(ErrorCode.POST_LIKE_DUPLICATED,
 			MessageFormat.format("이미 좋아요를 누른 게시글입니다. (postLikeId: {0})", postLikeId));
 	}
 
 	public static PostException NOT_FOUND_POST_LIKE(Long postId, Long memberId) {
-		throw new PostException(ErrorCode.NOT_FOUND_POST_LIKE,
+		return new PostException(ErrorCode.NOT_FOUND_POST_LIKE,
 			MessageFormat.format("좋아요를 누르지 않은 게시글은 좋아요를 취소할 수 없습니다. (postId: {0}, memberId: {1})",
 				postId, memberId));
 	}
 
 	public static PostException INVALID_EDITOR(Long creatorId, Long editorId) {
-		throw new PostException(ErrorCode.INVALID_EDITOR,
+		return new PostException(ErrorCode.INVALID_EDITOR,
 			MessageFormat.format("작성자와 수정자가 같지 않으면 삭제 할 수 없습니다. (creatorId: {0}, editorId: {1})",
 				creatorId, editorId));
 	}
 
 	public static PostException PARENT_COMMENT_NOT_FOUND(Long parentPostCommentId) {
-		throw new PostException(ErrorCode.NOT_FOUND_PARENT_POST_COMMENT,
+		return new PostException(ErrorCode.NOT_FOUND_PARENT_POST_COMMENT,
 			MessageFormat.format("상위 댓글이 존재하지 않습니다. (parentPostCommentId: {0})", parentPostCommentId));
 	}
 
 	public static PostException COMMENT_NOT_FOUND(Long postCommentId) {
-		throw new PostException(ErrorCode.NOT_FOUND_POST_COMMENT,
+		return new PostException(ErrorCode.NOT_FOUND_POST_COMMENT,
 			MessageFormat.format("댓글이 존재하지 않습니다. (postCommentId: {0})", postCommentId));
 	}
 }
